354f9131c3006b2ab97dcd3db650ec0c2083db5c
Michał Jezierski <ambinanitelo@gmail.com>
2018-12-19 08:14:37 +0100

    running tests

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const webpackConfig = require('./webpack.config');
 const jest = require('jest');
 
 const compiler = Webpack(webpackConfig);
-const devServerOptions = Object.assign({}, webpackConfig.devServer, {
-    stats: {
-        colors: true
-    }
-});
+const devServerOptions = Object.assign({}, webpackConfig.devServer);
 const server = new WebpackDevServer(compiler, devServerOptions);
 
 server.listen(8080, '127.0.0.1', () => {
-    console.log('Starting server on http://localhost:8080');
-
-    jest.run(['test.js', '-u']);
+    jest
+        .run(['--detectOpenHandles', 'test.js'])
+        .then(() => server.close());
 });
