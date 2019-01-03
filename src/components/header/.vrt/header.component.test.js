9073b279ec228e4b2c64c271fdce4a4b327746c1
Michał Jezierski <ambinanitelo@gmail.com>
2019-01-03 15:20:21 +0100

    work in progress

---
+++ src/components/header/.vrt/header.component.test.js
@@ -0,0 +1,26 @@
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+const puppeteer = require('puppeteer');
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('VRT', () => {
+    let browser;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+    });
+
+    it('600', async () => {
+        const page = await browser.newPage();
+        await page.setViewport({ width: 600, height: 400 });
+
+        await page.goto('http://localhost:57227/header.component.html');
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+});
