46093771b0ca71e04e03b278783b52e218d77225
Michał Jezierski <ambinanitelo@gmail.com>
2019-01-09 12:53:32 +0100

    work in progress

--- example/src/components/badge-with-props/index.js
+++ example/src/components/badge-with-props/index.js
@@ -4,7 +4,12 @@ import styles from './styles.css';
 export default function Badge ({ num = 0 }) {
     return (
         <div className={ styles.badge }>
-            { num > 0 ? num : 'no' } new messages
+            <span className={ styles.num }>
+                { num > 0 ? num : 'no' }
+            </span>
+            <span className={ styles.text }>
+                new message{ num !== 1 ? 's' : ''}
+            </span>
         </div>
     );
 }
