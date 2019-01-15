0d70950bc017fb3417c68f2becab13137b1280df
Michał Jezierski <ambinanitelo@gmail.com>
2019-01-15 12:38:47 +0100

    test script

--- examples/test.sh
+++ examples/test.sh
@@ -2,4 +2,4 @@
 
 STATUS=0
 
-find ./examples -depth 1 -type d -exec sh -c 'cd -- "{}" && npm i && npm t && STATUS=$(($STATUS+$?))' \;
+find './examples' -depth 1 -type d -exec sh -c 'cd -- "{}" && npm i && npm t && STATUS=$(($STATUS+$?))' \;
