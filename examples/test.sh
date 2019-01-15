edcbe47714cc3d02fa9d8c395ca42ac40578a01d
Michał Jezierski <ambinanitelo@gmail.com>
2019-01-15 12:27:55 +0100

    test script

--- examples/test.sh
+++ examples/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CURRENT_DIR=`dirname "$0"`
+CURRENT_DIR=$(dirname "$0")
 STATUS=0
 
-find $CURRENT_DIR -depth 1 -type d -exec sh -c 'cd -- "{}" && npm i && npm t && STATUS=$(($STATUS+$?))' \;
+find "$CURRENT_DIR" -depth 1 -type d -exec sh -c 'cd -- "{}" && npm i && npm t && STATUS=$(($STATUS+$?))' \;
