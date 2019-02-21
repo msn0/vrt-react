f4c34fc79b23a789b17f191515413e78ef6bb232
Michał Jezierski <ambinanitelo@gmail.com>
2019-02-21 08:35:12 +0100

    custom config path in examples

--- examples/test.sh
+++ examples/test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+npm link
+
 for dir in `ls -d $PWD/examples/*/`;
 do
     cd $dir
-    npm i && npm t
+    npm i && npm link @vrt/react && npm t
     STATUS=$?
     if [ $STATUS -ne 0 ]; then
         exit $STATUS
